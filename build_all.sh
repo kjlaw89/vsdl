04799ca0eb853da183fd9a66001bd30db1b74a21
Delyan Angelov <delian66@gmail.com>
2021-08-05 11:59:14 +0300

    add a build_all.sh script to ease testing that the project still compiles cleanly

---
+++ build_all.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+## stop execution as soon as some command fails, also show them as they are run:
+set -ex
+
+v examples/audio
+v examples/controller
+v examples/gfx
+v examples/gl
+v examples/image
+v examples/messagebox
+v examples/mixer
+v examples/system
+v examples/ttf
+v examples/tvintris
